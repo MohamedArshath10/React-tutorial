4086cb23764715199f0727c401d4bd01708e56de
MohamedArshath10 <arshath.m2003@gmail.com>
2025-01-25 15:03:09 +0530

    Tutorial completed

--- Tutorial-5(React-router)/src/Components/Navbar.jsx
+++ Tutorial-5(React-router)/src/Components/Navbar.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import "./Navbar.css"
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 function Navbar() {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate("/users")
+  }
   return (
     <nav>
         <h1>Arshath</h1>
         <ul>
             <Link to="/" className='link'>Home</Link>
-            <Link to="/users" className='link'>User</Link>
+            {/* <Link to="/users" className='link'>User</Link> */}
+            <li onClick={handleClick}>User</li>
             <Link to="/about" className='link'>About</Link>
             <Link to="/use" className='link'>Profile</Link>
         </ul>
