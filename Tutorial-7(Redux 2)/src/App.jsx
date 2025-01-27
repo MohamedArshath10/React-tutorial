18876a65f895cbcd8db46cf73486d3f1daecc486
MohamedArshath10 <arshath.m2003@gmail.com>
2025-01-27 13:51:03 +0530

    Redux examples

---
+++ Tutorial-7(Redux 2)/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CustomerAdd from './CustomerAdd'
+
+function App() {
+  return (
+    <>
+    <h1>React Redux</h1>
+      <CustomerAdd></CustomerAdd>
+    </>
+  )
+}
+
+export default App
