f99f4bcffa4358c6070c2c1cc7b7a2d15838b37a
MohamedArshath10 <arshath.m2003@gmail.com>
2025-01-31 12:21:00 +0530

    react query is completed

--- Tutorial-10(React Query)/src/Test1.jsx
+++ Tutorial-10(React Query)/src/Test1.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { useQuery } from 'react-query'
 
 const Test1 = () => {
+  const {isLoading, data, error} = useQuery('gitHubData',apiCall )
+
+  function apiCall(){
+    
+      return fetch('https://api.github.com/repos/MohamedArshath10/React-tutorial').then((res) => res.json())
+    }
+  
+  if (isLoading) return <div>Loading...</div>
+
+  if (error) return <h1>An error occured {error}</h1>
+
+  console.log(data);
   return (
-    <div>Test1</div>
+    <div>
+      <h1>Repo name: {data.name}</h1>
+      <h1>Repo sub count: {data.subscribers_count}</h1>
+      <h1>Stargazers: {data.stargazers_count}</h1>
+      <h1>Visibility: {data.visibility}</h1>
+    </div>
   )
 }
 
